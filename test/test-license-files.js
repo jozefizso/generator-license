881bd923b1c989f1c7b2e8da28a4396923580024
Jozef Izso <jozef.izso@gmail.com>
2016-01-24 14:44:53 +0100

    Updated yeoman-generator library and fixed tests to use new yeoman-test package.

--- test/test-license-files.js
+++ test/test-license-files.js
@@ -3,7 +3,7 @@
 'use strict';
 var path = require('path');
 var assert = require('yeoman-assert');
-var helpers = require('yeoman-generator').test;
+var helpers = require('yeoman-test');
 var process = require('process');
 
 describe('license:app templates', function () {
