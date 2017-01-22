7d8f7d683fdbd704ce2e6f00a8d86ce735ee19fa
ek9 <dev@ek9.co>
2017-01-22 02:27:06 +0100

    add eslint support via gulp with xo-space style

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,20 @@
 'use strict';
 var path = require('path');
 var gulp = require('gulp');
+var eslint = require('gulp-eslint');
 var excludeGitignore = require('gulp-exclude-gitignore');
 var mocha = require('gulp-mocha');
 var istanbul = require('gulp-istanbul');
 var plumber = require('gulp-plumber');
 var coveralls = require('gulp-coveralls');
+
+gulp.task('static', function () {
+  return gulp.src('**/*.js')
+    .pipe(excludeGitignore())
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+});
 
 gulp.task('pre-test', function () {
   return gulp.src('app/**/*.js')
@@ -40,4 +49,4 @@ gulp.task('watch', function () {
   gulp.watch(['app/**/*.js', 'test/**'], ['test']);
 });
 
-gulp.task('default', ['test']);
+gulp.task('default', ['static', 'test']);
